abc2263ab2a7132617f9b4509a02089a5855c8c9
aliaafathy <aliaaf518@gmail.com>
2022-04-24 21:46:27 +0200

    lol

--- src/Calculations/GraphFlow.java
+++ src/Calculations/GraphFlow.java
@@ -3,27 +3,27 @@ package Calculations;
 import java.util.LinkedList;
 
 public class GraphFlow {
-    LinkedList<Edge>[] adjacencylist;
+    LinkedList<Edge>[] Graphlist;
 
     public GraphFlow(int vertices) {
-        adjacencylist = new LinkedList[vertices];
+        Graphlist = new LinkedList[vertices];
         addVertex();
     }
     public void addVertex()
     {
-        for(int i=0; i<adjacencylist.length; i++)
+        for(int i = 0; i< Graphlist.length; i++)
         {
-            adjacencylist[i] = new LinkedList<Edge>();
+            Graphlist[i] = new LinkedList<Edge>();
         }
     }
     public void addEgde(Vertex source, Vertex destination, int weight) {
         Edge edge = new Edge(source, destination, weight);
-        adjacencylist[source.getId()].addFirst(edge); //for directed graph
+        Graphlist[source.getId()].addFirst(edge); //for directed graph
     }
 
-    /* public void printGraph() {
-         for (int i = 0; i < adjacencylist.length; i++) {
-             LinkedList<Edge> list = adjacencylist[i];
+    /* public void debugGraph() {
+         for (int i = 0; i < Graphlist.length; i++) {
+             LinkedList<Edge> list = Graphlist[i];
              for (int j = 0; j < list.size(); j++) {
                  System.out.println("vertex-" + list.get(j).source.getName() + " index " + list.get(j).source.getId() + "Boolean : " + list.get(j).source.isOutput()+ " is connected to " +
                          list.get(j).destination.getName() + " index " + list.get(j).destination.getId()+ " Boolean: " + list.get(j).destination.isOutput()+ " with weight " + list.get(j).weight);
@@ -31,12 +31,6 @@ public class GraphFlow {
          }
      }*/
 
-    /////////////////NOT yET ana ely 3amla/////////////////////
-   /* public String printResults()
-    {
-        TransferFunctionFinder finder = new TransferFunctionFinder(this);
-        finder.analyzeSignalFlowGraph();
-        return finder.showAnalysisInGUI();
-    }*/
+
 
 }
